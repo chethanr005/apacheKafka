c7b4dc7e03c7b25f048893a4255385cdf29bf1f4
chethanr005 <chethan.r005@gmail.com>
2024-09-16 07:51:02 -0700

    first commit

--- kafka/src/main/java/com/chethan/kafka/Dummy.java
+++ kafka/src/main/java/com/chethan/kafka/Dummy.java
@@ -3,5 +3,6 @@ package com.chethan.kafka;
 public class Dummy {
     public static void main(String[] args) {
         System.out.println("This is a kafka project");
+        System.out.println("This is a second kafka project");
     }
 }
